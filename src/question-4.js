44ee6481dab70b7f7d9f75b30221f98a0fdbcee4
kajirioji <JcNNUXf27yaNPyU>
2021-03-30 18:51:48 -0600

    Finish question 6 and 4.

--- src/question-4.js
+++ src/question-4.js
@@ -2,20 +2,28 @@ function question4() {
     const statement = '4. Describe at least 3 design patterns and 2 anti-patterns.\n';
     console.log(statement);
     console.log('Design patterns\n');
+
     console.log('Factory');
-    console.log('Define an interface for creating a single object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.');
+    console.log('It is a creational pattern in which an interface to create a single object is defined, but it let subclasses decide which class to instantiate. It is considered an alternative to constructors. Unlike constructors they can return an object of any subtype of their return type. It is used when it is not know what concrete class it will be required at runtime but it is wanted to get a class that will do the job.\n');
+
+    console.log('Module');
+    console.log('The idea with this structural design pattern is to group related elements (classes, methods, other modules, etc.) into a single entity. It provides structure and helps organize the code. Its implementation depends highly on the programming language and framework.\n');
+
+    console.log('Guarded suspension');
+    console.log('It is a concurrent design pattern that allows an operation until a condition is met, if not it remains locked. It involves suspending the method call and the calling thread until the condition is satisfied.\n');
+
 
     console.log('\nDesign anti-patterns\n');
-    console.log('Singleton'); //https://stackoverflow.com/questions/12755539/why-is-singleton-considered-an-anti-pattern
-    console.log('Ensure a class has only one instance, and provide a global point of access to it.');
-    console.log('it is overused, introduces unnecessary restrictions in situations where a sole instance of a class is not actually required, and introduces global state into an application');
-    console.log('In short, the singleton pattern makes code more complex, less useful, and a real pain to re-use or test. Eliminating singletons can be tricky, but it’s a worthwhile endeavour.');
-    /*
-    https://sourcemaking.com/antipatterns/software-development-antipatterns
 
-    * Cut-and-Paste Programming
-Code reused by copying source statements leads to significant maintenance problems. Alternative forms of reuse, including black-box reuse, reduce maintenance issues by having common source code, testing, and documentation.
-* */
+    console.log('Singleton Anti-Pattern');
+    console.log('Ensure a class has only one instance, and provide a global point of access to it. When it is misused, it introduces unnecessary restrictions in situations where a sole instance of a class is not actually required, and introduces global state into an application. The singleton pattern makes code more complex, less useful, and can become a bottleneck during development and debugging.\n');
+
+    console.log('Cut-And-Paste Programming');
+    console.log('It creates code duplication, bugs reoccurs all over the code. It would be easier to create reusable code.\n');
+
+    console.log('Silver Bullet (Golden hammer)');
+    console.log('It happens when it is thought that one solution is suitable for all the problems (especially when speaking about programming language, frameworks or even design patterns). As a result of Golden hammer is poorly performing, redundant overcomplicated code.\n');
+
 }
 
 question4();
--- src/question-6.js
+++ src/question-6.js
@@ -8,7 +8,6 @@ let dishesDelivering = 0;
 let dishesDelivered = 0;
 let availableChefs = 3;
 let availableWaiters = 1;
-let seconds = 0;
 
 function question6() {
     const statement = '6. There are N chefs and M waiters, they should prepare and deliver exactly K dishes. ' +
@@ -22,11 +21,6 @@ function question6() {
         output: process.stdout
     });
 
-    rl.on("close", function() {
-        console.log("\nBYE!!");
-        process.exit(0);
-    });
-
     rl.question("What is the amount of dishes to serve ? ", function(inputDishes) {
         rl.question("How many chefs are available ? ", function(inputChefs) {
             rl.question("How many waiters are available ? ", function(inputWaiters) {
@@ -34,85 +28,74 @@ function question6() {
                 console.log("");
 
                 dishes = parseInt(inputDishes);
-                dishesInQueue = dishes;
                 availableChefs = parseInt(inputChefs);
                 availableWaiters = parseInt(inputWaiters);
 
                 if( isNaN(dishes) || isNaN(availableChefs) || isNaN(availableWaiters)) {
                     console.error("Invalid parameters.");
-                    rl.close();
+                } else if ( availableChefs === 0 ) {
+                    console.error("No chefs available.");
+                } else if ( availableWaiters === 0 ) {
+                    console.error("No waiters available.");
+                } else if ( dishes === 0 ) {
+                    console.error("No dishes to serve.");
                 }
 
-                console.log(`Chefs: ${availableChefs}`);
-                console.log(`Waiters: ${availableWaiters}`);
-                console.log(`Dishes: ${dishes} \n`);
+                rl.close();
 
-                startPreparing().then(r => {
-                    rl.close();
-                });
+                dishesInQueue = dishes;
+                console.log(`Dishes: ${dishes}`);
+                console.log(`Chefs: ${availableChefs}`);
+                console.log(`Waiters: ${availableWaiters} \n`);
+
+                startPreparing();
             });
         });
     });
-
 }
 
-async function startPreparing() {
-    // console.log("Start");
+function startPreparing() {
 
     let currentDishPreparing = 0;
     let currentDishDelivering = 0;
+    let seconds = 0;
+
     console.log(`Dish in queue: ${dishesInQueue} - Preparing: ${dishesPreparing} - Prepared: ${dishesPrepared} - Delivering: ${dishesDelivering} - Delivered: ${dishesDelivered} - Available Chefs: ${availableChefs} - Available Waiters: ${availableWaiters}`);
     function checkDishesChefsWaiters() {
         if (dishesDelivered < dishes) {
+
+            seconds++;
+            console.log(`Time (sec): ${seconds}`);
+
             while (availableChefs > 0 && dishesInQueue > 0) {
-                console.log(`Prepare dish #${++currentDishPreparing}`);
+                console.log(`   Prepare dish #${++currentDishPreparing}`);
                 dishesInQueue--;
                 dishesPreparing++;
                 availableChefs--;
                 const worker = new Worker(__filename, {workerData: 'chef'});
-                worker.on('message', (msg) => {
-                    // console.log(msg);
+                worker.on('message', () => {
                     dishesPreparing--;
                     dishesPrepared++;
                     availableChefs++;
                 });
             }
             while (availableWaiters > 0 && dishesPrepared > 0) {
-                console.log(`Deliver dish #${++currentDishDelivering}`);
+                console.log(`   Deliver dish #${++currentDishDelivering}`);
                 dishesPrepared--;
                 availableWaiters--;
                 dishesDelivering++;
                 const worker = new Worker(__filename, {workerData: 'waiter'});
-                worker.on('message', (msg) => {
-                    // console.log(msg);
+                worker.on('message', () => {
                     dishesDelivering--;
                     dishesDelivered++;
                     availableWaiters++;
                 });
             }
-            seconds++;
-            console.log(`Time (sec): ${seconds}`);
             console.log(`Dish in queue: ${dishesInQueue} - Preparing: ${dishesPreparing} - Prepared: ${dishesPrepared} - Delivering: ${dishesDelivering} - Delivered: ${dishesDelivered} - Available Chefs: ${availableChefs} - Available Waiters: ${availableWaiters}`);
             setTimeout(checkDishesChefsWaiters, 1000);
         }
-
-        //
-        // if(availableChefs === 0 || availableWaiters === 0) {
-        //     setTimeout(checkAvailableChefs, 1000);
-        // } else {
-        //     if(dishesInQueue>0 && availableChefs>0){
-        //         prepareDish();
-        //     }
-        //     if(dishesPrepared>0 && availableWaiters>0){
-        //         deliverDish();
-        //     }
-        // }
-        //setTimeout(checkAvailableChefs, 1000);
     }
     checkDishesChefsWaiters();
-
-    // await sleep(2000);
-    // console.log("End")
 }
 
 function sleep(ms) {
@@ -123,12 +106,12 @@ function sleep(ms) {
 
 async function prepareDish() {
     const preparingDish = dishes - dishesInQueue + 1;
-    console.log(`Preparing dish: #${preparingDish}`);
+    console.log(`   Preparing dish: #${preparingDish}`);
     dishesInQueue--;
     dishesPreparing++;
     availableChefs--;
     await sleep(1000);
-    console.log(`Prepared dish: #${preparingDish}`);
+    console.log(`   Prepared dish: #${preparingDish}`);
     dishesPreparing--;
     dishesPrepared++;
     availableChefs++;
@@ -136,12 +119,12 @@ async function prepareDish() {
 
 async function deliverDish() {
     const preparingDish = dishes - dishesInQueue + 1;
-    console.log(`Delivering dish: #${preparingDish}`);
+    console.log(`   Delivering dish: #${preparingDish}`);
     dishesPrepared--;
     availableWaiters--;
     dishesDelivering++;
     await sleep(1000);
-    console.log(`Delivered dish: #${preparingDish}`);
+    console.log(`   Delivered dish: #${preparingDish}`);
     dishesDelivering--;
     dishesDelivered++;
     availableWaiters++;
@@ -149,31 +132,11 @@ async function deliverDish() {
 
 
 if (isMainThread) {
-    // This code is executed in the main thread and not in the worker.
-    // console.log("isMainThread");
-
-    /*
-    // Create the worker.
-    const worker = new Worker(__filename);
-    // Listen for messages from the worker and print them.
-    worker.on('message', (msg) => { console.log(msg); });
-    */
-    // question6();
-    // const worker = new Worker(__filename, {workerData: 'chef'});
-    // worker.on('message', (msg) => { console.log(`Message from worker: ${msg}`); });
-    startPreparing();
+    question6();
+    //startPreparing();
 
 } else {
-    // console.log("worker");
-    // This code is executed in the worker and not in the main thread.
-    // Send a message to the main thread.
     sleep(500).then(r => {
         parentPort.postMessage(workerData);
     });
-    //
-    // if(workerData === "chef") {
-    //     prepareDish();
-    // } else if(workerData === "waiter") {
-    //     deliverDish();
-    // }
 }
